54cf4ef39a22114290995dcfe7c63adeea892751
BraisonCrece <104019638+BraisonCrece@users.noreply.github.com>
2024-06-19 00:04:38 +0200

    feat: add ExerciseCard component and refactor exercise handling (#16)
    - Added new `ExerciseCard` component to handle video exercises with a loading state.
    - Replaced `ShowVideoComponent` with `ExerciseCard` in `ExercisesPage`.
    - Integrated GraphQL query for fetching exercises in `ExercisesPage`.
    - Removed unused `ShowVideoComponent` from codebase.
    - Updated `CoachProtectedRoute` and `ProtectedRoute` to include `redirect` from `react-router-dom`.
    - Enhanced `ErrorPage` with a "Go back" button for user navigation.
    - Improved routing structure in `routes.jsx`, removing unnecessary loader and example.

--- src/pages/coach/exercises/ExercisesPage.jsx
+++ src/pages/coach/exercises/ExercisesPage.jsx
@@ -1,25 +1,38 @@
 import React from "react"
-import { useLoaderData } from "react-router-dom"
-import ShowVideoComponent from "../../../components/ShowVideoComponent"
+import ExerciseCard from "../../../components/ExerciseCard"
+import { EXERCISES_QUERY } from "../../../graphql/queries/coach/exercises"
+import { useQuery } from "@apollo/client"
 
 export default function ExercisesPage() {
-  const { exercises } = useLoaderData()
-  console.log(exercises)
+  const { loading, error, data } = useQuery(EXERCISES_QUERY, {
+    fetchPolicy: "network-only"
+  })
+
+  if (error) return <p>Error: {error.message}</p>
+
+  const { exercises } = data || {}
 
   return (
     <div className="container mx-auto px-4 mt-9 mb-24">
       <h1 className="text-2xl font-bold mb-4">Exercises</h1>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {exercises.map(exercise => (
-          <div key={exercise.id}>
-            <ShowVideoComponent
-              videoUrl={`http://localhost:3000/${exercise.videoUrl}`}
-              title={exercise.title}
-              description={exercise.description}
-            />
-          </div>
-        ))}
-      </div>
+      {loading ? (
+        <div className="flex justify-center items-center h-64">
+          <p>Loading...</p> {/*  */}
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+          {exercises.map((exercise, key) => (
+            <div key={key}>
+              <ExerciseCard
+                videoStatus={exercise.videoStatus}
+                videoUrl={`http://localhost:3000/${exercise.videoUrl}`}
+                title={exercise.title}
+                description={exercise.description}
+              />
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
