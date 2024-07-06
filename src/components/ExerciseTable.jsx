aad67044709a59f713854f95bce1a76edc9dfa80
BraisonCrece <104019638+BraisonCrece@users.noreply.github.com>
2024-07-06 17:33:30 +0200

    feat: add exercise management and pagination (#19)
    - Added `ExerciseTable` component to display exercises in a table format
    - Created GraphQL mutations `DELETE_EXERCISE_MUTATION` and `UPDATE_EXERCISE_MUTATION`
    - Updated `EXERCISES_QUERY` to support pagination
    - Enhanced `ExercisesPage` to include paginated querying, next and previous
     page navigation, and improved UI components for exercise management

---
+++ src/components/ExerciseTable.jsx
@@ -0,0 +1,60 @@
+import React from "react"
+
+function ExerciseTable({ exercises, ITEMS_PER_PAGE }) {
+  return (
+    <table className="w-full border-collapse rounded overflow-hidden shadow-sm">
+      <caption className="hidden">Exercises list</caption>
+      <thead className="bg-gray-100 border-collapse text-left">
+        <tr className="border-collapse">
+          <th className="w-2/6 max-w-12 px-8 py-4">Title</th>
+          <th className="w-3/6 px-8 py-4">Description</th>
+          <th className="w-1/6 px-8 py-4 text-center">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {exercises.map(({ node: exercise }, key) => (
+          <tr key={key} className="even:bg-gray-50 border-collapse">
+            <td className="max-w-12 w-2/6 px-8 py-4 truncate overflow-ellipsis whitespace-nowrap">
+              {exercise.title}
+            </td>
+            <td className="w-3/6 px-8 py-4 truncate overflow-ellipsis whitespace-nowrap">
+              {exercise.description}
+            </td>
+            <td className="px-8 py-4 flex justify-evenly">
+              <div className="mr-2">
+                <button
+                  className="text-blue-500"
+                  id={exercise.id}
+                  onClick={() => {
+                    alert(`Details for exercise ${exercise.id}`)
+                  }}>
+                  Details
+                </button>
+              </div>
+              <div className="mr-2">
+                <button
+                  className="text-red-500"
+                  id={exercise.id}
+                  onClick={() => {
+                    alert(`Delete exercise ${exercise.id}`)
+                  }}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        ))}
+        {exercises.length < ITEMS_PER_PAGE &&
+          [...Array(ITEMS_PER_PAGE - exercises.length)].map((_, index) => (
+            <tr key={`placeholder-${index}`} className="even:bg-gray-50">
+              <td className="w-2/6 max-w-12 px-8 py-4">&nbsp;</td>
+              <td className="w-3/6 px-8 py-4">&nbsp;</td>
+              <td className="w-1/6 px-8 py-4">&nbsp;</td>
+            </tr>
+          ))}
+      </tbody>
+    </table>
+  )
+}
+
+export default ExerciseTable
--- src/pages/coach/exercises/ExercisesPage.jsx
+++ src/pages/coach/exercises/ExercisesPage.jsx
@@ -1,37 +1,97 @@
-import React from "react"
-import ExerciseCard from "../../../components/ExerciseCard"
-import { EXERCISES_QUERY } from "../../../graphql/queries/coach/exercises"
+import React, { useState, useEffect } from "react"
 import { useQuery } from "@apollo/client"
+import { EXERCISES_QUERY } from "../../../graphql/queries/coach/exercises"
+import ExerciseTable from "../../../components/ExerciseTable"
 
 export default function ExercisesPage() {
-  const { loading, error, data } = useQuery(EXERCISES_QUERY, {
+  const ITEMS_PER_PAGE = 10
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const { loading, error, data, fetchMore } = useQuery(EXERCISES_QUERY, {
+    variables: { first: ITEMS_PER_PAGE, after: null, last: null, before: null },
     fetchPolicy: "network-only"
   })
 
   if (error) return <p>Error: {error.message}</p>
 
   const { exercises } = data || {}
+  const edges = exercises?.edges || []
+  const pageInfo = exercises?.pageInfo || {}
+  const totalCount = exercises?.totalCount || 0
+
+  const handleNextPage = () => {
+    fetchMore({
+      variables: {
+        first: ITEMS_PER_PAGE,
+        after: pageInfo.endCursor,
+        before: null,
+        last: null
+      },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        if (!fetchMoreResult) return prev
+        setCurrentPage(currentPage + 1)
+        return fetchMoreResult
+      }
+    })
+  }
+
+  const handlePreviousPage = () => {
+    console.log("startCursor (handlePrevious)", pageInfo.startCursor)
+    fetchMore({
+      variables: {
+        after: null,
+        first: null,
+        before: pageInfo.startCursor,
+        last: ITEMS_PER_PAGE
+      },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        if (!fetchMoreResult) return prev
+        setCurrentPage(currentPage - 1)
+        return fetchMoreResult
+      }
+    })
+  }
+
+  const totalPages = Math.ceil(totalCount / ITEMS_PER_PAGE)
 
   return (
     <div className="container mx-auto px-4 mt-9 mb-24">
       <h1 className="text-2xl font-bold mb-4">Exercises</h1>
+
       {loading ? (
         <div className="flex justify-center items-center h-64">
-          <p>Loading...</p> {/*  */}
+          <p>Loading...</p>
         </div>
       ) : (
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-          {exercises.map((exercise, key) => (
-            <div key={key}>
-              <ExerciseCard
-                videoStatus={exercise.videoStatus}
-                videoUrl={`http://localhost:3000/${exercise.videoUrl}`}
-                title={exercise.title}
-                description={exercise.description}
-              />
+        <>
+          <section className="flex justify-between items-center mb-4 gap-4">
+            <input
+              type="search"
+              className="w-2/3 border border-gray-100 px-4 py-2 mb-4 rounded"
+              placeholder="Search exercises"
+            />
+            <div className="grow flex justify-center mb-4 gap-2">
+              <button
+                className="rounded p-2 w-24 bg-slate-100 text-blue-500"
+                onClick={handlePreviousPage}
+                disabled={!pageInfo.hasPreviousPage}>
+                Previous
+              </button>
+              <button
+                className="rounded p-2 w-24 bg-slate-100 text-blue-500"
+                onClick={handleNextPage}
+                disabled={!pageInfo.hasNextPage}>
+                Next
+              </button>
             </div>
-          ))}
-        </div>
+            <div>
+              <span>
+                Page {currentPage} of {totalPages}
+              </span>
+            </div>
+          </section>
+          <ExerciseTable exercises={edges} ITEMS_PER_PAGE={ITEMS_PER_PAGE} />
+        </>
       )}
     </div>
   )
